f00cb5a33df499a5a26da45f42677b97e8914f0e
Aidan Host <aidan@greenman.digital>
2025-12-28 19:49:59 +0200

    chore: use IO thread

--- app/src/main/java/digital/greenman/silverumbrella/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/digital/greenman/silverumbrella/data/repository/WeatherRepositoryImpl.kt
@@ -6,31 +6,35 @@ import digital.greenman.silverumbrella.data.remote.WeatherApi
 import digital.greenman.silverumbrella.domain.model.WeatherDetails
 import digital.greenman.silverumbrella.domain.repository.WeatherRepository
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 private const val TAG = "WeatherRepositoryImpl"
 
 class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherRepository {
     override suspend fun getCurrentWeather(lat: Double, lon: Double): Result<WeatherDetails> {
-        return try {
-            val response = weatherApi.getCurrentWeather(lat, lon)
-            val data = response.body()
+        return withContext(Dispatchers.IO) { // Switch to IO thread
+            try {
+                val response = weatherApi.getCurrentWeather(lat, lon)
+                val data = response.body()
 
-            if (response.isSuccessful && data != null) {
-                Result.success(data.toDomain())
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Log.e(TAG, "Failed to fetch weather data: ${response.code()} - $errorBody")
-
-                if (response.code() == 404) {
-                    Result.failure(Exception("Location not found."))
+                if (response.isSuccessful && data != null) {
+                    Result.success(data.toDomain())
                 } else {
-                    Result.failure(Exception("Failed to fetch weather data: ${response.message()}"))
+                    val errorBody = response.errorBody()?.string()
+                    Log.e(TAG, "Failed to fetch weather data: ${response.code()} - $errorBody")
+
+                    if (response.code() == 404) {
+                        Result.failure(Exception("Location not found."))
+                    } else {
+                        Result.failure(Exception("Failed to fetch weather data: ${response.message()}"))
+                    }
                 }
+            } catch (e: Exception) {
+                if (e is CancellationException) throw e
+                Log.e(TAG, "Exception fetching weather data", e)
+                Result.failure(e)
             }
-        } catch (e: Exception) {
-            if (e is CancellationException) throw e
-            Log.e(TAG, "Exception fetching weather data", e)
-            Result.failure(e)
         }
     }
 }
